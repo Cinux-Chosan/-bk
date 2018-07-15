c81827777f91d65074e84dac91682b5a8d9313eb
Chosan <179817004@qq.com>
2018-07-16 00:00:22 +0800

    .

---
+++ tests/unit/utils/local-storage-test.ts
@@ -0,0 +1,11 @@
+import localStorage from 'amazon/utils/local-storage';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | localStorage', function(hooks) {
+
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = localStorage();
+    assert.ok(result);
+  });
+});
